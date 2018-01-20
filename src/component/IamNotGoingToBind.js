2304bbe52f1ab9cb84274c1b880fa16f409c339d
Ali Emami <emamiali1993@gmail.com>
2018-01-20 14:12:03 -0800

    added a commnet

--- src/component/IamNotGoingToBind.js
+++ src/component/IamNotGoingToBind.js
@@ -4,10 +4,13 @@ class IamNotGoingToBind extends Component {
   state = {
     counter: 0
   }
+  //this is binding automagically 
   render() {
     return (
       <div  style={{ margin: '10px', padding: '10px'}}>
-        <button style={{fontSize: '20px'}} onClick={() => { this.setState({counter: this.state.counter + 1}); console.log('not Binded!!: ', this.state.counter);}}>
+        <button
+          style={{fontSize: '20px'}}
+          onClick={() => { this.setState({counter: this.state.counter + 1}); console.log('not Binded!!: ', this.state.counter);}}>
           Not Binded!!!!
         </button>
       </div>
